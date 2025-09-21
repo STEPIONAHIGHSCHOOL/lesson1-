f8f56bf2cff267a209e3d515ca14b20941806191
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2025-09-20 22:51:35 -0400

    lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+# Store a name inside a variable​
+name = "Alex"​
+
+#Store age inside a variable​
+
+age= 16​
+
+score = 95 # store score inside a variable​
+
+print(name, age, score) #print name, age, and score​
